cefc4406c8a91cd2f4f68e7b674986142fb0bec0
David Song <david.jh.song@gmail.com>
2025-06-21 19:51:38 +0900

    rvert

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,7 +16,6 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: '../server/dist',
-    emptyOutDir: true,
+    outDir: path.resolve(__dirname, '../server/dist')
   }
 })
