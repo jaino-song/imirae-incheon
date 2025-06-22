c8a1d3c4773338bcb70f73689d4ce57b36fa54bd
David Song <david.jh.song@gmail.com>
2025-06-22 10:34:22 +0900

    Refactor for separate Vercel deployments
    - Remove static file serving from Express server
    - Remove root vercel-build script
    - Aligns codebase with a separate client/server project setup on Vercel.

--- server/server.js
+++ server/server.js
@@ -49,15 +49,6 @@ app.use(express.json());
 const eformsignRoutes = require('./routes/eformsign');
 app.use('/api', eformsignRoutes);
 
-// Serve static files from the React app build
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
